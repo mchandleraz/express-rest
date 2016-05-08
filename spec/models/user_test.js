2f5432c58054b459201473d32b03ab96b10bf57b
bstil22 <brady.stilwell@csaa.com>
2016-05-07 20:41:25 -0700

    setup jasmine.json and migrate specs to jasmine syntax

--- spec/models/user_test.js
+++ spec/models/user_test.js
@@ -1,115 +1,121 @@
 var app 		= require('../../app.js');
 var baseUrl 	= app.get('baseUrl');
+var User = require('../../models/user');
 
 describe('MODELS', function () {
 
-  var User = require('../../models/user');
-  console.log('hh')
   describe('User', function() {
-  console.log('hh')
-    it('throws an error if password.length < 12', function () {
+    it('throws an error if password.length < 12', function (done) {
       var user = {
         username: 'foo4',
         password: 'barbar'
       };
 
       User.create(user, function (err) {
-        console.log(err);
-        console.log('fuck');
         expect(err).toBeDefined();
+        expect(err).not.toBeNull();
+        done();
       });
     });
 
-  //  it('throws an error if username.length < 4', function () {
-  //    var user = {
-  //      username: 'foo',
-  //      password: 'barbarfoofooasdf'
-  //    };
-  //
-  //    User.create(user, function (err) {
-  //      should.exist(err);
-  //    });
-  //
-  //  });
-  //
-  //  it('throws an error if password is missing', function () {
-  //    var user = {
-  //      username: 'foo4',
-  //      password: null
-  //    };
-  //
-  //    User.create(user, function (err) {
-  //      should.exist(err);
-  //    });
-  //  });
-  //
-  //  it('throws an error if username is missing', function () {
-  //    var user = {
-  //      username: null,
-  //      password: 'barbarfoofooasdf'
-  //    };
-  //
-  //    User.create(user, function (err) {
-  //      should.exist(err);
-  //    });
-  //  });
-  //
-  //  it('persists a User if data is valid', function () {
-  //    var validUser = {
-  //      username: 'testUser' + Date.now(),
-  //      password: 'passwordwoooo',
-  //      admin: false
-  //    };
-  //
-  //    User.create(validUser, function (err) {
-  //      should.not.exist(err);
-  //    });
-  //  });
-  //
-  //  it('trims the username', function () {
-  //    var validUser = {
-  //      username: 'userfoo  ',
-  //      password: 'passwordwoooo',
-  //      admin: false
-  //    };
-  //
-  //    User.create(validUser, function (err, createdUser) {
-  //      createdUser.username.should.equal('userfoo');
-  //    });
-  //  });
-  //
-  //  it('trims the password', function () {
-  //    var validUser = {
-  //      username: 'userfoo',
-  //      password: 'passwordwoooo   ',
-  //      admin: false
-  //    };
-  //
-  //    User.create(validUser, function (err, createdUser) {
-  //      createdUser.password.should.equal('passwordwoooo');
-  //    });
-  //  });
-  //});
-  //
-  //describe('Static Methods', function () {
-  //
-  //  it('encryptPassword: encrypts the password', function () {
-  //
-  //    var user = new User();
-  //    var password = 'passwordwoooo';
-  //    var encryptedPassword = user.encryptPassword(password);
-  //
-  //    encryptedPassword.should.not.equal(password);
-  //  });
-  //
-  //  it('decryptPassword: decrypts the password', function () {
-  //
-  //    var user = new User();
-  //    var password = 'passwordwoooo';
-  //    var encryptedPassword = user.encryptPassword(password);
-  //    var decryptedPassword = user.decryptPassword(password, encryptedPassword);
-  //
-  //    decryptedPassword.should.be.true;
-  //  });
+    it('throws an error if username.length < 4', function (done) {
+      var user = {
+        username: 'foo',
+        password: 'barbarfoofooasdf',
+        admin: true
+      };
+
+      User.create(user, function (err) {
+        expect(err).not.toBeNull();
+        done();
+      });
+    });
+
+    it('throws an error if password is missing', function (done) {
+      var user = {
+        username: 'foo4',
+        password: null
+      };
+
+      User.create(user, function (err) {
+        expect(err).toBeDefined();
+        expect(err).not.toBeNull();
+        done();
+      });
+    });
+
+    it('throws an error if username is missing', function (done) {
+      var user = {
+        username: null,
+        password: 'barbarfoofooasdf'
+      };
+
+      User.create(user, function (err) {
+        expect(err).toBeDefined();
+        expect(err).not.toBeNull();
+        done();
+      });
+    });
+
+    it('persists a User if data is valid', function (done) {
+      var validUser = {
+        username: 'testUser' + Date.now(),
+        password: 'passwordwoooo',
+        admin: false
+      };
+
+      User.create(validUser, function (err) {
+        expect(err).toBeNull();
+        done();
+      });
+    });
+
+    it('trims the username', function (done) {
+      var validUser = {
+        username: 'userfoo  ',
+        password: 'passwordwoooo',
+        admin: false
+      };
+
+      User.create(validUser, function (err, createdUser) {
+        expect(createdUser.username).toEqual('userfoo');
+        done();
+      });
+    });
+
+    it('trims the password', function (done) {
+      var validUser = {
+        username: 'userfoo',
+        password: 'passwordwoooo   ',
+        admin: false
+      };
+
+      User.create(validUser, function (err, createdUser) {
+        expect(createdUser.password).toEqual('passwordwoooo');
+        done();
+      });
+    });
   });
+
+  describe('Static Methods', function () {
+
+    it('encryptPassword: encrypts the password', function () {
+
+      var user = new User();
+      var password = 'passwordwoooo';
+      var encryptedPassword = user.encryptPassword(password);
+
+      expect(encryptedPassword).not.toEqual(password);
+    });
+
+    it('decryptPassword: decrypts the password', function () {
+
+      var user = new User();
+      var password = 'passwordwoooo';
+      var encryptedPassword = user.encryptPassword(password);
+      var decryptedPassword = user.decryptPassword(password, encryptedPassword);
+
+      expect(decryptedPassword).toBe(true);
+    });
+  })
 });
